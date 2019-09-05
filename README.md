099368c20ea62aa0c31b0b4b8a6ed2a53a937d2f
André Kunitz <andrekunitz@gmail.com>
2019-09-05 20:47:50 -0300

    Set up DB connect and add Product Schema

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ An API build with Node.js following the course *NodeJS* by *Rocketseat*.
 ## What I Learned
 - Create project structure; 
 - Create routes with Express.js framework;
+- Use Nodemon for development; 
--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+// Initialize app
 const app = express();
 
+// Connect to DB
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    { useNewUrlParser: true }
+);
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
+// Create first route
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with  react',
+        url: 'https://github.com/facebook/react-native'
+    });
+    
+    return res.send('Hello World!');
 })
 
 app.listen(3001);
